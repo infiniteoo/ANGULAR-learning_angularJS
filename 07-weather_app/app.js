d47e0fac891993ef2b1889341b70d6899ad73e1c
Troy Dorman <troydorman@gmail.com>
2021-04-21 11:30:58 -0600

    finished this crummy course

---
+++ 07-weather_app/app.js
@@ -0,0 +1,30 @@
+// MODULE
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+// ROUTES
+
+/* weatherApp.config(function($routeProvider) {
+
+    $routeProvider
+
+    .when('/', {
+        templateUrl: 'pages/home.htm',
+        controller: 'homeController'
+
+    })
+
+    .when('/forecast', {
+        templateUrl: '/pages/forecase.htm',
+        controller: 'forecaseController'
+    })
+
+});
+
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', function($scope){
+
+}]);
+
+weatherApp.controller('forecastController', ['$scope', function($scope){
+
+}]); */
